bc18c737e89ff05de808083fbd5816679247f689
Sa Hyun Hwang <4hyun8@gmail.com>
2022-08-16 22:26:45 +0900

    init comit

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,16 @@
+create TABLE spaces(
+    space_id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    owner VARCHAR(30) NOT NULL
+);
+create sequence space_id_seq;
+create TABLE messages(
+    space_id INT NOT NULL REFERENCES spaces(space_id),
+    msg_id INT PRIMARY KEY,
+    author VARCHAR(30) NOT NULL,
+    msg_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    msg_text VARCHAR(1024) NOT NULL
+);
+create sequence msg_id_seq;
+create INDEX msg_timestamp_idx ON messages(msg_time);
+create UNIQUE INDEX space_name_idx ON spaces(name);
